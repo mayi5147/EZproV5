4e59627c378cdb2890be6bafff1f6db77f236cff
mayi5147 <92247557+mayi5147@users.noreply.github.com>
2022-08-13 23:20:13 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -14,7 +14,7 @@ uci set network.lan.ipaddr='192.168.101.1'                                    # 
 uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
 uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci set dhcp.@dnsmasq[0].filter_aaaa='1'                                # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
-uci set system.@system[0].hostname='EZproV5'                            # 修改主机名称为OpenWrt-123
+uci set system.@system[0].hostname='EZProV5'                            # 修改主机名称为OpenWrt-123
 EOF
 
 
